b916db8f1e0d80df737ad1f5e7efb02b2e7958bc
JunsoonHwang <wnstns13@gmail.com>
2021-06-14 18:26:36 +0900

    ch9-5

---
+++ CH9/05/main05.js
@@ -0,0 +1,21 @@
+// let isShow = true;
+// let checked = false;
+
+// if (isShow) {
+//   console.log('Show!'); // 참 = 출력
+// }
+
+// if (checked) {
+//   console.log('checked!'); // 거짓 = 출력X
+// }
+
+let isShow = true;
+
+if (isShow) {
+  console.log('Show!');
+}
+
+else { // 참이 아닐시 
+  console.log('Hide?')
+}
+
