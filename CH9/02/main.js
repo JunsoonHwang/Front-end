b9da103d5b2df8da2cde319cc626490faedba39c
JunsoonHwang <wnstns13@gmail.com>
2021-06-14 18:24:39 +0900

    ch9-2

---
+++ CH9/02/main.js
@@ -0,0 +1,16 @@
+// let myName = "Junear"; // let 이름이라는 변수 선언
+// let email = 'wnstns13@naver.com'
+// let hello = `${myName}?!` // ``(백틱 = 보관법), ${}로 숫자,데이터들을 불러올수있음 
+
+// console.log(myName); // console.log();로 웹사이트 개발자도구로 불러옴
+// console.log(email);
+// console.log(hello);
+
+let user = {
+  name: 'Junear', // ,써주고 다음 내용
+  age: 85
+};
+
+console.log(user);
+console.log(user.age); // user안에 들어있는 age라는 속성값을 출력하겠다
+console.log(user.name);
