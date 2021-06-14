2c7ee17ff474cd7b28e3f3dc71f9fab41838acc4
JunsoonHwang <wnstns13@gmail.com>
2021-06-14 18:27:37 +0900

    ch9-t1

---
+++ CH9/t1/main.js
@@ -0,0 +1,50 @@
+// A
+
+// 1. theQuickBrownFox 
+// // 첫단어 소문자 그 이후 단어 시작마다 대문자
+
+// 2. console.log(fruit[1]);
+// // [] = 배열 데이터
+
+// 3. false
+// // JS에서 불린 데이터 = true, false
+
+// 4. object(객체 데이터)
+
+// 5. 인수
+
+// 6. const hello = function() {};
+//    hello();
+
+// 7. 메소드
+
+// 8. if(true) {}
+
+// 9. defer
+
+// 10. const boxEl = document.querySelector('.box');
+// console.log(boxEl.textContent);
+// //박스라는 클래스를 찾아서 박스엘리먼트에 할당
+// //텍스트 컨텐트라는 속성을 이용해 반환된 값을 콘솔에 출력
+
+// 11. boxEl.addEventListener('click', function() {
+//   console.log('hello~');
+// });
+// //박스 엘리먼트에서 클릭이라는 이벤트가 발생하면 핸들러 부분의 함수를 실행을 한다.
+// //클릭시 콘솔창에 Hello~라는 문자 데이터를 출력함
+
+// 13. const divEls = document.querySelectorAll('div')
+//     divEls.forEach(function(divEl) {
+//       divEl.classList.add('hello');
+//     });
+
+// //html에서 div태그라는 요소들을 전부 찾아서 전부 반복적으로 실행해서
+// //각각의 요소에 hello라는 클래스를 추가하는 코드
+
+// 14.메소드 체이닝 //.메소드('').메소드().메소드('')
+
+// 15.if(boxEl.classList.contains('active')) {
+//   console.log('포함됨!');
+// }
+// //엑티브라는 클래스가 박스엘리먼트에 포함되어 있으면 true
+// //참일시 중괄호에 있는 내용이 실행이됌
